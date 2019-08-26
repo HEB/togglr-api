a6a190ae00a1f60b088e7e8df012d042c1a5b3db
g592345 <gorena.david@heb.com>
2019-08-26 14:07:45 -0500

    Updates for Open Source.

--- azureDeploy.sh
+++
@@ -1,4 +0,0 @@
-az acr login --name sadcNonProdContainerRegistry &&
-docker build -t togglr-api . &&
-docker tag togglr-api sadcnonprodcontainerregistry.azurecr.io/togglr-api:stable &&
-docker push sadcnonprodcontainerregistry.azurecr.io/togglr-api:stable
--- gcpDeloy.sh
+++
@@ -1,4 +0,0 @@
-gcloud auth configure-docker &&
-docker build -t togglr-api:stable . &&
-docker tag togglr-api:stable gcr.io/heb-cx-labs/togglr-api:stable &&
-docker push gcr.io/heb-cx-labs/togglr-api:stable
